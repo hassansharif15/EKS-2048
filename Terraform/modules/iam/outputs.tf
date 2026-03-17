fad75f490558d46d17683644701f055d668e5fcb
hassansharif15 <hassansharif15@gmail.com>
2026-03-17 15:06:06 +0000

    add github actions workflows and monitoring files

--- Terraform/modules/iam/outputs.tf
+++ Terraform/modules/iam/outputs.tf
@@ -7,12 +7,9 @@ output "node_role_arn" {
   description = "EKS node group IAM role ARN"
   value       = aws_iam_role.node_role.arn
 }
-
-module "iam" {
-  source            = "./modules/iam"
-  project_name      = var.project_name
-  oidc_provider_arn = module.eks.oidc_provider_arn
-  oidc_provider_url = module.eks.oidc_provider_url
+output "certmanager_role_arn" {
+  description = "IAM role ARN for CertManager"
+  value       = aws_iam_role.certmanager_role.arn
 }
 
 output "externaldns_role_arn" {
